4ddd71e3cd591f4991006c83bf20eb1320629d13
MDan.Twenty.One <>
2022-04-14 23:40:29 +0300

    Тестовый коммит. Добавил файл Map.py с классом Map.

---
+++ Map.py
@@ -0,0 +1,10 @@
+# This is class Map is used for storing info about all objects in match except players
+class Map:
+	def __init__(self):
+		self.__objects = {'Units': [], 'Buildings': []}
+		# Levels Of Platforms
+		self.lop = {0: [], 1: [], 2: [], 3: []}
+
+	def add(self, obj):
+		# Пока так
+		self.__objects['Units'].append(obj)
