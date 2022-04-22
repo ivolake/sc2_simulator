f12591eac567fb3c90807e0bc007db87c20fce5e
CyTb_On_Fire <cytb2024@gmail.com>
2022-04-23 00:29:34 +0300

    22.04.2022 Добавлены абстрактные описания относящихся к Match классов и методов.
    Match.py: Инициализирован базовый класс для всех изменяемых объектов на карте, установлено поле "HitPoints" для этих объектов; Добавлены прототипы основных функций разграничения этапов матча.

--- Match.py
+++ Match.py
@@ -1,5 +1,52 @@
-#This is Match class
+# TODO: Создать методы : "start"(Начало матча) - создание карты, как объекта Map и стартовых изменяющихся объектов
+#  с их начальными параметрами;
+#  "End"(Завершение матча) - ;
+#  Связать систему приказов, юнитов и здания
+#  Модель симуляции в Match.proceed - итерация по всем изменяющимся объектам на карте.
+#  Для таких объектов создать класс Object (BasicObject, ...) от которого будут наследоваться все остальные
+#  изменяющиеся объекты на карте: Unit, Building, Destroyable.
+#  Изменяться они будут посредством накопленных в буффере в течение предыдущих итераций команд.
+
+from sc2_simulator import Players
+
+
+class ObjectBase:
+    __hitPoints: int
+
+    @property
+    def HitPoints(self) -> int:
+        return self.__hitPoints
+
+
+
 
 class Match:
+    __frequency: int
+    Player_1 = Players.Player()
+    Player_2 = Players.Player()
+
+    def gameIteration(self):
+        #цикл для перебора всех команд очереди, а также обновления состояния соответствующих объектов, задействованных в итерации
+        pass
+
+
     def __init__(self):
         print("So, it begins")
+
+    def refresh(self):
+#Должны быть обновлены свойства юнитов в соответствии со стеком команд за данную итерацию
+        pass
+
+    def start(self):
+        #Все стартовые свойства должны быть установлены на соответствующии значения
+        #Все стартовые объекты с обоих сторон должны быть сгенерированы
+        pass
+
+    def proceed(self):
+        Match.game_Iteration()
+
+        pass
+
+    def end(self):
+        #условие завершения матча
+        pass
--- Players.py
+++ Players.py
@@ -1,3 +1,5 @@
 #здесь будут описаны взаимодействия с юнитами и игроками
 class Player:
+    def __init__(self):
+        print("created")
     pass
