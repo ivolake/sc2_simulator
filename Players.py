f559b2d51d317e5f49f409628fd2cca7776190e7
Kirri7 <86826650+Kirri7@users.noreply.github.com>
2022-04-22 21:21:14 +0300

    Добавлена проверка ника у Player.
    Players.py: добавлена функция верификации и обрезки никнеймов verify_nickname.

--- Players.py
+++ Players.py
@@ -1,3 +1,41 @@
-#здесь будут описаны взаимодействия с юнитами и игроками
+# TODO: сделать ветку, СИСТЕМУ ПРИКАЗОВ
+#  добавить имя игрока, добавить клан, добавить панель приказов(координаты move),
+#  Команды: idle, hold, attack, move, use ability(type of ability), patrol
+# 3.1. send_unit_cmd - функция, описывающее доставку до юнита определенной команды. Оперирует объектами класса Unit и их полями current_command.
+# 3.2 send_unit_move, send_unit_attack - подвиды функции отправки команды, отправляющие конкретные команды юниту.
+
+
 class Player:
-    pass
+    """
+    Нужен для связи пользователей с юнитами
+    """
+    def __init__(self, name: str):
+        self.player_name = self.verify_nickname(name)
+
+    def verify_nickname(self, nick):
+        """
+        Проверка ника
+        """
+        banlist_of_letters = ["\\",":"]
+        if len(nick) > 15:
+            print(f"Слишком длинный ник, замена на {nick[:15]}.")
+            nick = nick[:15]
+
+        for letter in banlist_of_letters:
+            if letter in nick:
+                raise ValueError(f"Запрещённые символы в нике: '{letter}'.")
+
+        return nick
+
+
+
+
+
+class Command:
+    """
+
+    """
+    #move
+    #attack
+    def __init__(self):
+        pass
